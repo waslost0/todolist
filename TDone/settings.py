577fde5a96ca2b0d48608c42c733e8432ce02c32
Mark <marcus.koh16.01.97@gmail.com>
2020-10-27 19:16:28 +0300

    popup delete form

--- TDone/settings.py
+++ TDone/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'tasks',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -120,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
--- tasks/forms.py
+++ tasks/forms.py
@@ -5,7 +5,7 @@ from .models import Task
 
 class TaskForm(ModelForm):
     title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Add new task...'}))
-    
+
     class Meta:
         model = Task
         fields = "__all__"
